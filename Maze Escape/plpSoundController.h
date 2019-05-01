a295febbb5a03b1d771571275913440389d26f4f
Paul Ronga <paul.ronga@laposte.net>
2019-05-01 15:39:02 +0200

    audio: add mute setter & getter

--- Maze Escape/plpSoundController.h
+++ Maze Escape/plpSoundController.h
@@ -8,3 +8,14 @@
 
 #import <Foundation/Foundation.h>
 #import <SpriteKit/SpriteKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AVFoundation/AVAudioSession.h>
+
+@interface plpSoundController : NSObject {
+    SKAudioNode *platformSound;
+    BOOL muteMusic;
+    BOOL muteSoundFX;
+}
+- (BOOL)isHeadsetPluggedIn;
+- (void)initSounds;
+@end
--- Maze Escape/plpSoundController.m
+++ Maze Escape/plpSoundController.m
@@ -7,3 +7,54 @@
 //
 
 #import "plpSoundController.h"
+
+/*
+ manages:
+ - when sound gets muted
+ - when sound should / should not get played
+ 
+*/
+
+@implementation plpSoundController
+
+- (id)init
+{
+    NSLog(@"init sound controller");
+
+    self = [super init];
+    if (!self) return nil;
+
+    return self;
+}
+
+- (BOOL)isHeadsetPluggedIn {
+    AVAudioSessionRouteDescription* route = [[AVAudioSession sharedInstance] currentRoute];
+    for (AVAudioSessionPortDescription* desc in [route outputs]) {
+        if ([[desc portType] isEqualToString:AVAudioSessionPortHeadphones])
+            return YES;
+    }
+    return NO;
+}
+
+- (void)setMuteMusic: (BOOL) muted{
+    self -> muteMusic = muted;
+}
+- (void)setMuteSoundFX: (BOOL) muted{
+    self -> muteSoundFX = muted;
+}
+- (BOOL)getMuteMusic{
+    return self -> muteMusic;
+}
+- (BOOL)getMuteSoundFX{
+    return self -> muteSoundFX;
+}
+
+- (void)initSounds {
+    platformSound = [[SKAudioNode alloc] initWithFileNamed:@"Sounds/fx_elevateur.wav"];
+    [self->platformSound runAction: [SKAction play]];
+    NSLog(@"end init inner func");
+}
+
+
+
+@end
